d6a14aa8dbeadca64113d7ceb6dbde01bccbed91
vsaran93 <vishnusaran37@gmail.com>
2017-05-29 18:31:40 +0530

    initial commit

---
+++ upload/system/framework.php
@@ -0,0 +1,150 @@
+<?php
+// Registry
+$registry = new Registry();
+
+// Config
+$config = new Config();
+$config->load('default');
+$config->load($application_config);
+$registry->set('config', $config);
+
+// Log
+$log = new Log($config->get('error_filename'));
+$registry->set('log', $log);
+
+set_error_handler(function($code, $message, $file, $line) use($log, $config) {
+	// error suppressed with @
+	if (error_reporting() === 0) {
+		return false;
+	}
+
+	switch ($code) {
+		case E_NOTICE:
+		case E_USER_NOTICE:
+			$error = 'Notice';
+			break;
+		case E_WARNING:
+		case E_USER_WARNING:
+			$error = 'Warning';
+			break;
+		case E_ERROR:
+		case E_USER_ERROR:
+			$error = 'Fatal Error';
+			break;
+		default:
+			$error = 'Unknown';
+			break;
+	}
+
+	if ($config->get('error_display')) {
+		echo '<b>' . $error . '</b>: ' . $message . ' in <b>' . $file . '</b> on line <b>' . $line . '</b>';
+	}
+
+	if ($config->get('error_log')) {
+		$log->write('PHP ' . $error . ':  ' . $message . ' in ' . $file . ' on line ' . $line);
+	}
+
+	return true;
+});
+
+// Event
+$event = new Event($registry);
+$registry->set('event', $event);
+
+// Event Register
+if ($config->has('action_event')) {
+	foreach ($config->get('action_event') as $key => $value) {
+		$event->register($key, new Action($value));
+	}
+}
+
+// Loader
+$loader = new Loader($registry);
+$registry->set('load', $loader);
+
+// Request
+$registry->set('request', new Request());
+
+// Response
+$response = new Response();
+$response->addHeader('Content-Type: text/html; charset=utf-8');
+$registry->set('response', $response);
+
+// Database
+if ($config->get('db_autostart')) {
+	try {
+		$registry->set('db', new DB($config->get('db_type'), $config->get('db_hostname'), $config->get('db_username'), $config->get('db_password'), $config->get('db_database'), $config->get('db_port')));
+	} catch(Exception $e) {
+		$response->redirect($config->get('site_base') . 'maintenance.html');
+	}
+}
+
+// Session
+$session = new Session();
+
+if ($config->get('session_autostart')) {
+	$session->start();
+}
+
+$registry->set('session', $session);
+
+// Cache 
+$registry->set('cache', new Cache($config->get('cache_type'), $config->get('cache_expire')));
+
+// Url
+if ($config->get('url_autostart')) {
+	$registry->set('url', new Url($config->get('site_base'), $config->get('site_ssl')));
+}
+
+// Language
+$language = new Language($config->get('language_default'));
+$language->load($config->get('language_default'));
+$registry->set('language', $language);
+
+// Document
+$registry->set('document', new Document());
+
+// Config Autoload
+if ($config->has('config_autoload')) {
+	foreach ($config->get('config_autoload') as $value) {
+		$loader->config($value);
+	}
+}
+
+// Language Autoload
+if ($config->has('language_autoload')) {
+	foreach ($config->get('language_autoload') as $value) {
+		$loader->language($value);
+	}
+}
+
+// Library Autoload
+if ($config->has('library_autoload')) {
+	foreach ($config->get('library_autoload') as $value) {
+		$loader->library($value);
+	}
+}
+
+// Model Autoload
+if ($config->has('model_autoload')) {
+	foreach ($config->get('model_autoload') as $value) {
+		$loader->model($value);
+	}
+}
+
+// Front Controller
+$controller = new Front($registry);
+
+// Pre Actions
+if ($config->has('action_pre_action')) {
+	foreach ($config->get('action_pre_action') as $value) {
+		$controller->addPreAction(new Action($value));
+	}
+}
+
+// Dispatch
+$controller->dispatch(new Action($config->get('action_router')), new Action($config->get('action_error')));
+
+// Output
+$response->setCompression($config->get('config_compression'));
+$response->output();
